a09f91f6dc1fd613719b2877b76903e525d47b6e
N0tR1CH <damianrichter58@gmail.com>
2024-08-17 22:14:06 +0200

    Finish description textarea editor

--- scripts/src/app.ts
+++ scripts/src/app.ts
@@ -1,10 +1,23 @@
 import htmxLib from "htmx.org/dist/htmx.esm.js";
 import Socket from "./socket.js";
 import _hyperscript from "hyperscript.org";
+import EasyMDELib from "easymde";
+import Alpine from "alpinejs";
+
+declare global {
+  interface Window {
+    htmx: typeof htmx;
+    Alpine: typeof Alpine;
+  }
+}
 
 window.addEventListener("DOMContentLoaded", (): void => {
-  Socket.init();
   window.htmx = htmxLib;
+  window.EasyMDE = EasyMDELib;
+  window.Alpine = Alpine;
+
+  Socket.init();
+  Alpine.start();
   _hyperscript.browserInit();
 
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
--- scripts/types/global.d.ts
+++
@@ -1,7 +0,0 @@
-declare global {
-  interface Window {
-    htmx: typeof htmx;
-  }
-}
-
-export {};
